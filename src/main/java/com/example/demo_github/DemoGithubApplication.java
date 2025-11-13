02cb9f1733a3a7630498d1b42d1e19266396c75e
Moustapha124 <123896968+Moustapha124@users.noreply.github.com>
2025-11-13 11:54:33 +0000

    Update DemoGithubApplication.java

--- src/main/java/com/example/demo_github/DemoGithubApplication.java
+++ src/main/java/com/example/demo_github/DemoGithubApplication.java
@@ -3,6 +3,8 @@ package com.example.demo_github;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 @SpringBootApplication
 public class DemoGithubApplication {
 
